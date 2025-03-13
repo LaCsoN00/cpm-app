8655e681e8414449bd88b87807d0926c9de801c5
Lacson <lucmouguengui@gmail.com>
2025-03-13 21:14:18 +0100

    Fix modifications apportées

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import withPWA from '@ducanh2912/next-pwa';
 
 const nextConfig = withPWA({
     dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
     register: true,
+    sw: 'service-worker.js',
   },
 );
 
