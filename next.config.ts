5bebdf7d71b117dc77a038143be0c98059458256
Lacson <lucmouguengui@gmail.com>
2025-03-13 20:14:58 +0100

    Fix modifications apportées

--- next.config.ts
+++ next.config.ts
@@ -7,3 +7,34 @@ export default withPWA({
     skipWaiting: true,      
     sw: "service-worker.js"
 });
+
+export async function headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, proxy-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+      {
+        source: '/manifest.json',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/manifest+json',
+          },
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+        ],
+      },
+  ];
+}
