f6fe03e84682b54578408ec4a19c4c090a139798
Lacson <lucmouguengui@gmail.com>
2025-03-13 19:18:05 +0100

    Fix modifications apportées

--- next.config.ts
+++ next.config.ts
@@ -1,39 +1,9 @@
-import withPWA from "@ducanh2912/next-pwa";
+import withPWA from 'next-pwa';
 
-const nextConfig = withPWA({
-  dest: "public",
+export default withPWA({
+    dest: "public",   
+    disable: process.env.NODE_ENV === "development",       
+    register: true,         
+    skipWaiting: true,      
+    sw: "service-worker.js"
 });
-
-const headers = async () => {
-  return [
-    {
-      source: "/sw.js",
-      headers: [
-        {
-          key: "Cache-Control",
-          value: "no-store, no-cache, must-revalidate, proxy-revalidate",
-        },
-        {
-          key: "Service-Worker-Allowed",
-          value: "/",
-        },
-      ],
-    },
-    {
-      source: "/manifest.json",
-      headers: [
-        {
-          key: "Content-Type",
-          value: "application/manifest+json",
-        },
-        {
-          key: "Cache-Control",
-          value: "public, max-age=0, must-revalidate",
-        },
-      ],
-    },
-  ];
-};
-
-export default nextConfig;
-export { headers };
